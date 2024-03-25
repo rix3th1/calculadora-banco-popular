5d2a50b01733673c1ef0523e26bff43e7e86d293
Richard A. Rich <rojasricor@gmail.com>
2024-03-25 15:59:29 -0500

    Banco Popular theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import CalcForm from "@/components/CalcForm";
 import FormInputs from "@/components/FormInputs";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center flex-col">
-      <h1 className="font-bold lg:text-4xl mt-10 mb-5 text-center sm:text-2xl text-zinc-700">
-        Calculadora de capital Banco Popular
+      <Image
+        className="mt-14 m-10"
+        src="/bp-logotipo-horizontal.svg"
+        alt="logo"
+        width={250}
+        height={250}
+      />
+
+      <h1 className="font-normal lg:text-xl mb-5 text-center sm:text-2xl text-zinc-600">
+        Simulador de capital de inversión{" "}
+        <span className="font-bold text-[#11B719]">Banco Popular</span>
       </h1>
 
       <CalcForm>
