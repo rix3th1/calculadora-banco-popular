b3b0c68845b8583c27115b95b4be539ea1c936e3
Richard A. Rich <rojasricor@gmail.com>
2024-03-25 15:59:48 -0500

    Banco Popular theme

--- src/libs/actions.ts
+++ src/libs/actions.ts
@@ -8,7 +8,5 @@ export async function calculate(_: unknown, formData: FormData) {
   const days = Number(formData.get("days"));
   const version = formData.get("version") as Version;
 
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
   return calc(capital, days, version); // return the result of the calculation function to the client.
 }
